55e2c6da8bf4f667a0c3dd215a9eae860a68ba82
Justin <justinreeves52@gmail.com>
2021-01-08 22:50:09 -0500

    Added more intervals, can change direction and replay interval

--- src/script.js
+++ src/script.js
@@ -3,46 +3,72 @@ import {Interval, Note} from '@tonaljs/tonal';
 const notes = ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5'];
 let score = 0;
 let waiting = false;
-// Intervals
+// Button factory
+const Button = (id, interval) => {
+    const button = document.createElement("button");
+    button.id = id;
+    button.innerText = interval;
+    button.addEventListener("click", (e) => {
+        checkResponse(Intervals.interval, e.target.id)
+    })
+    return button;
+}
 const Intervals = {
+    type: "melodic",
     interval: "",
-    diatonic: ["M2", "M3", "P4", "P5", "M6", "M7", "P8"],
+    start: "",
+    direction: "asc",
+    intervals: ["m2", "M2", "m3", "M3", "P4", "P5", "m6", "M6", "m7", "M7", "P8"],
     makeButtons: () => {
         const body = document.getElementsByTagName("body")[0];
         
+        const controlDiv = document.createElement("div");
+
         const playInterval = document.createElement("button");
-
-        const major2 = document.createElement("button");
-        const major3 = document.createElement("button");
-        const perfect4 = document.createElement("button");
-        const perfect5 = document.createElement("button");
-        const major6 = document.createElement("button");
-        const major7 = document.createElement("button");
-        const octave = document.createElement("button");
-        
+        const replayInterval = document.createElement("button");
+        const changeDirection = document.createElement("button");
+        controlDiv.append(playInterval, replayInterval, changeDirection);
+        const intervalDiv = document.createElement("div");
+        intervalDiv.append(
+            Button("m2", "Minor 2nd"),
+            Button("M2", "Major 2nd"),
+            Button("m3", "Minor 3rd"), 
+            Button("M3", "Major 3rd"),
+            Button("P4", "Perfect 4th"),
+            Button("P5", "Perfect 5th"),
+            Button("m6", "Minor 6th"),
+            Button("M6", "Major 6th"),
+            Button("m7", "Minor 7th"),
+            Button("M7", "Major 7th"),
+            Button("P8", "Octave"));
+        intervalDiv.id = "intervals";
         playInterval.innerText = "Start";
+        replayInterval.innerText = "Replay";
+        changeDirection.innerText = "Change Direction";
         playInterval.addEventListener("click", () => Intervals.playRandom());
-        major2.innerText = "Major 2nd";
-        major2.id = "M2";
-        major3.innerText = "Major 3rd";
-        major3.id = "M3";
-        perfect4.innerText = "Perfect 4th";
-        perfect4.id = "P4";
-        perfect5.innerText = "Perfect 5th";
-        perfect5.id = "P5";
-        major6.innerText = "Major 6th";
-        major6.id = "M6";
-        major7.innerText = "Major 7th";
-        major7.id = "M7";
-        octave.innerText = "Octave";
-        octave.id = "P8";
-        body.append(playInterval, major2, major3, perfect4, perfect5, major6, major7, octave);
-
-        let buttons = [major2, major3, perfect4, perfect5, major6, major7, octave];
-        buttons.forEach(btn => btn.addEventListener("click", (e) => {
-            checkResponse(Intervals.interval,e.target.id);
-            }
-        ));
+        replayInterval.addEventListener("click", () => Intervals.playInterval(Intervals.interval))
+        changeDirection.addEventListener("click", () => Intervals.changeDirection());
+        body.append(controlDiv, intervalDiv);
+    },
+    changeDirection: () => {
+        if (Intervals.direction == "asc") {
+            Intervals.direction = "desc";
+        } else {
+            Intervals.direction = "asc";
+        }
+    },
+    playInterval: (interval) => {
+        let end;
+        // Determine end depending on if direction of interval is ascending or descending
+        if (Intervals.direction == "asc") {
+            end = Note.transpose(Intervals.start,interval);
+        } else end = Note.transpose(Intervals.start, interval.substr(0,1) + '-' + interval.substr(1));
+        // Play interval
+        let audioCtx = new AudioContext();
+        Soundfont.instrument(audioCtx, 'acoustic_grand_piano').then((piano) => {
+            piano.play(Intervals.start, audioCtx.currentTime).stop(audioCtx.currentTime + 1);
+            piano.play(end, audioCtx.currentTime + 1).stop(audioCtx.currentTime + 2);
+        });
     },
     playRandom: () => {
         
@@ -51,10 +77,14 @@ const Intervals = {
         }
         // Get starting note
         const start = notes[getRandom(notes.length)];
+        Intervals.start = start;
         // Get interval        
-        let interval = Intervals.diatonic[getRandom(Intervals.diatonic.length)];
+        let interval = Intervals.intervals[getRandom(Intervals.intervals.length)];
         // Get second note in interval
-        const end = Note.transpose(start,interval);
+        let end;
+        if (Intervals.direction == "asc") {
+            end = Note.transpose(start,interval);
+        } else end = Note.transpose(start, interval.substr(0,1) + '-' + interval.substr(1));
         // Play interval
         let audioCtx = new AudioContext();
         Soundfont.instrument(audioCtx, 'acoustic_grand_piano').then((piano) => {
@@ -64,6 +94,32 @@ const Intervals = {
         Intervals.interval = interval;
         // Wait for user input, check response
         waiting = true;
+    },
+    convert: (intervalAbbr) => {
+        switch(intervalAbbr) {
+            case("M2"):
+                return "Major 2nd";
+            case("m2"):
+                return "Minor 2nd";
+            case("M3"):
+                return "Major 3rd";
+            case("m3"):
+                return "Minor 3rd";
+            case ("P4"):
+                return "Perfect 4th";
+            case("P5"):
+                return "Perfect 5th";
+            case("M6"):
+                return "Major 6th";
+            case("m6"):
+                return "Minor 6th";
+            case("M7"):
+                return "Major 7th";
+            case("m7"):
+                return "Minor 7th";
+            case("P8"):
+                return "Octave";
+        }
     }
 }
 Intervals.makeButtons();
@@ -72,13 +128,14 @@ function checkResponse(interval, response) {
     if (interval == response && waiting) {
         // Show user is correct
         score++;
-        document.getElementById("score").innerText = score;
+        let correct = `<br>Correct! You answered ${Intervals.convert(response)}`;
+        document.getElementById("score").innerHTML = score + correct;
         Intervals.playRandom();
     } else {
         // Show user is incorrect, show correct answer
         score--;
-        document.getElementById("score").innerText = score;
+        let incorrect = `<br>Incorrect. You answered ${Intervals.convert(response)} while the answer is ${Intervals.convert(interval)}`;
+        document.getElementById("score").innerHTML = score + incorrect;
         Intervals.playRandom();
-        return false;
     }
 }
