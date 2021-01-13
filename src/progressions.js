83502ee54a590c4cd99c32bf2bc2ec48e68a2f68
Justin <justinreeves52@gmail.com>
2021-01-13 01:00:01 -0500

    Added streak functionality for progressions, fixed bugs when removing and adding answers to the progression, added more response to a correct answer

--- src/progressions.js
+++ src/progressions.js
@@ -6,6 +6,13 @@ const Progressions = {
     progression: ["Imaj"],
     length: 4,
     answer: [],
+    /**
+     * 
+     * @param {string} chordName - name of one of the chords that could be
+     * played in the progression
+     * @return {HTMLButtonElement}
+     * @TODO make into class 
+     */
     progressionButton: (type) => {
         const button = document.createElement("button");
         button.id = type;
@@ -14,58 +21,83 @@ const Progressions = {
         return button;
     },
     addResponse: (e) => {
+        console.log(Progressions.answer.length)
         let answer = e.target.id;
-        console.log(answer);
+        let i;
         if (Progressions.answer.includes(null)) {
+            i = Progressions.answer.indexOf(null);
             Progressions.answer[Progressions.answer.indexOf(null)] = answer;
+            document.getElementById("answers").insertBefore(Progressions.responseSpan(answer, i), document.getElementById("answers").children[i]);
         } else {
             Progressions.answer.push(answer);
+            document.getElementById("answers").append(Progressions.responseSpan(answer, Progressions.answer.length - 1))
         }
-        document.getElementById("answers").append(Progressions.responseSpan(answer, Progressions.answer.length - 1));
-        console.log(Progressions.progression);
-        console.log(Progressions.answer);
+        // Check answer
         if (Progressions.progression.length == Progressions.answer.length) {
             Progressions.checkResponse();
         }
-     },
-     equal: (arr1, arr2) => {
-        
+    },
+    /**
+     * @param {string[]} arr1 Chord Progression randomly generated
+     * @param {string[]} arr2 Chord Progression identified by user
+     * @return {boolean} true if chord progressions are the same between the arrays
+     */
+    equal: (arr1, arr2) => {
+        if (arr1.length != arr2.length) {return false}
         for (let i = 0; i < arr1.length; i++) {
             if (arr1[i] != arr2[i]) return false;
         }
         return true;
-     },
+    },
     checkResponse: () => {
-        console.log(`Progression : ${Progressions.progression}`);
-        console.log(`Answer : ${Progressions.answer}`);
         if (Progressions.equal(Progressions.progression, Progressions.answer)) {
+            // Update score
             score++;
-            document.getElementById("score").innerText = score;
+            // Update HTML
+            document.getElementById("score").innerText = "Correct!";
+            setTimeout(() => {
+                document.getElementById("score").innerText = score;
+                document.getElementById("answers").innerText = "";
+            
+            }, 3000)
+            // Update array presenting user's responses
             Progressions.answer = [];
-            document.getElementById("answers").innerText = "";
+            // Set local storage if necessary
+             if (localStorage.getItem("progressionStreak") < score) {
+                localStorage.setItem("progressionStreak", score);
+                document.getElementById("streak").innerText = localStorage.getItem("progressionStreak");
+            }
+            // Play new progression
             Progressions.playRandom();
         } else {
+            score = 0;
+            document.getElementById("score").innerText = score;
             Progressions.answer.forEach((el, i) => {
-                if (el != Progressions.progression[i]) {
-                    document.getElementById(el).classList += " incorrect";
-                } 
+                if (el != Progressions.progression[i] && Progressions.answer[i] != null) {
+                    Array.from(document.getElementById("answers").children)[i].classList += " incorrect";
+                }
             })
         }
+
     },
+    /**
+     * 
+     * @param {string} answer - abbreviated name of the chord selected by the user
+     * @param {number} i - index of the chord within the Progression.answer array 
+     * @return {HTMLSpanElement} span element with text of the chord name, id, appropriate
+     * class name and event handler to remove node from selected chords
+     */
     responseSpan: (answer, i) => {
         let span = document.createElement("span");
         span.innerText = answer;
         span.id = answer;
         span.classList += "answer";
         // Remove from answers
-        span.addEventListener("click", () => {
-            Progressions.answer.splice(i, 1, null);
-            console.log(Progressions.answer);
-            Array.from(document.getElementById("answers").children).forEach((el, i) => {
-                if (el.id != Progressions.progression[i] && Progressions.progression[i] !=null) {
-                    el.remove();
-                }
-            });
+        span.addEventListener("click", (e) => {
+            if (e.target.id != Progressions.progression[i] && e.target.classList.contains("incorrect")) {
+                Progressions.answer.splice(i, 1, null);
+                e.target.remove();
+            }
         });
         return span;
     },
@@ -104,8 +136,10 @@ const Progressions = {
         }
         main.append(progressionDiv);
     },
+    /** Plays the randomly generated chord progression. 
+     * @see Progressions.playChord
+    */
     playProgression: () => {
-        console.log(Progressions.progression);
         for (let i = 0; i < Progressions.length; i++) {
             setTimeout(() => {
                 Progressions.playChord(Progressions.progression[i]);
@@ -121,6 +155,10 @@ const Progressions = {
             }
         })
     },
+    /**
+     * @param {string} chord - Abbreviated name for the chord 
+     * @return {string} Full name for the chord
+     */
     chordType: (type) => {
         if (new RegExp(/.+maj/).test(type)) {
             let a = type.replace(/(.+)maj/, '$1major');
@@ -130,6 +168,10 @@ const Progressions = {
             return b;
         } else return type;
     },
+    /** Generates a random chord progression from the available chords then calls
+     * Progressions.playProgression to play the progression.
+     *  @see Progressions.playProgression
+     */
     playRandom: () => {
         Progressions.progression = ["Imaj"];
         const getRandom = (max) => {
@@ -141,6 +183,12 @@ const Progressions = {
         Progressions.playProgression();
     },
     load: () => {
+        if (localStorage.getItem("progressionStreak") != null) {
+            document.getElementById("streak").innerText = localStorage.getItem("progressionStreak");
+        } else {
+            localStorage.setItem("progressionStreak", 0);
+        }
+
         Progressions.makeControls();
         Progressions.makeProgressionButtons();
     }
